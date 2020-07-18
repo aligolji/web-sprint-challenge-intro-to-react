43ba5100e1077e3bfd69fa5babc92aa1cbd3e033
unknown <ali-golji@lambdastudents.com>
2020-07-17 21:42:02 -0700

    got map to work etc. styling in progress

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,53 +1,44 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import './App.css';
-import CharacterList from './components/CharacterList';
+import Character from './components/Character';
 
-
-console.log('test')
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
-  const [character, setCharacter] = useState('');
+  const [characters, setCharacters] = useState([]);
 
-console.log('test 2')
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
 
   const axiosData = 'https://swapi.dev/api/people/';
 
-console.log('test 3')
   useEffect(() => {
     axios.get(axiosData)
-      .then(function (response) {
-        setCharacter(response.data.results)
-      
-        console.log(response.data.results)
-      }
-    )
-
-
-      .catch(function (error) {
-
-          console.log(error)
-        }
-      )
+      .then(response => {
+        setCharacters(response.data.results)
+      })    
+        
+      .catch(error => {
+      })
       
   }, []);
 
-
-
-
-
-
+  // console.log(characters)
 
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
-      { <CharacterList characters={character} /> }
+      {
+        characters.map(character => {
+          return <Character key={character.id} info={character} />
+        })
+      }
     </div>
   );
 }
+
+console.log('still working?')
 
 export default App;
--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -2,30 +2,31 @@
 import React from 'react';
 
 const Character = props => {
-
-    const {character} = props
-    console.log(character)
-
+console.log(props)
+// console.log(props.info.mass)
     return (
         <div className='character-card'>
-            <div className='basic-info'>
-                <h2>{character.name}</h2>
-                <span>Homeworld: + {'    '} + {character.homeworld}</span>
-                <span>Birth Year: + {'    '} + {character.birth_year}</span>                
+            
+            <div>
+                <span>{props.info.name}</span>
+                <span>Year of Birth:  {props.info.birth_year}</span>
             </div>
-
-            <div className='character-details'>
-                <span>Height: + {'    '} + {character.height}</span>
-                <span>Mass: + {'    '} + {character.mass}</span>
-                <span>Hair Color: + {'    '} + {character.hair_color}</span>
-                <span>Skin Color: + {'    '} + {character.skin_color}</span>
-                <span>Eye Color: + {'    '} + {character.eye_color}</span>
-                <span>Gender: + {'    '} + {character.gender}</span>
-                <span>Films: + {'    '} + {character.films}</span>
-                <span>Species: + {'    '} + {character.species}</span>
-                <span>Vehicles: + {'    '} + {character.vehicles}</span>
-                <span>Starships: + {'    '} + {character.starships}</span>
-            </div>
+            <div>
+                <span>Height: {props.info.height}</span>
+                <span>Mass:   {props.info.mass}</span>
+                <span>Hair Color: {props.info.hair_color}</span>
+                <span>Skin Color: {props.info.skin_color}</span>
+                <span>Eye Color:  {props.info.eye_color}</span>
+                <span>Gender: {props.info.gender}</span>
+                {/*
+                    homeworld
+                    films
+                    species
+                    starships
+                    vehicles
+                    these are arrays arrays... require get request for more data
+                */}
+            </div>                      
 
         </div>
 
@@ -33,3 +34,5 @@ const Character = props => {
 };
 
 export default Character;
+
+console.log('am i still working?')
